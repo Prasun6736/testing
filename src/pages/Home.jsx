48b610fa615b3b5aee34f3a681f351a2fdf2eade
Pratap2018 <pratapmridha@gmail.com>
2023-12-20 00:11:19 +0530

    update lang

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,61 @@
 // src/pages/Home.jsx
 import React from "react";
 import { Link } from "react-router-dom";
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
+import langHome from '../utils/lang'
 
 const Home = () => {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
+  const [switchState, setSwitchState] = React.useState(true);  
+  const [data,setLang]=React.useState({
+    lang : langHome.eng
+  })
+  function handleOnChange(e) {
+    switch (e.target.checked) {
+      case true:
+        setLang({
+          lang:langHome.eng
+        })
+        break;
+    case false:
+      setLang({
+        lang:langHome.hindi
+      })
+
+
+        break;
+    }
+    setSwitchState(!switchState);
+  }
 
   return (
     <>
       <div className="bg-slate-300 w-screen h-screen flex flex-col justify-center items-center gap-10">
+      
+      
+      <input 
+        id="checkbox" 
+        type="checkbox" 
+        checked={switchState}
+        onChange={handleOnChange} />  
+
+
+
         <h1 className="text-2xl font-black text-slate-700">
-          We Are under maintainance right now, please access your dashboard by{" "}
+          {data.lang.title}
         </h1>
         <div className="flex gap-10">
           <Link
             className="w-[220px] h-[100px] text-2xl bg-white rounded-xl flex justify-content items-center"
             to="login"
           >
-            Login
+            {data.lang.login}
           </Link>
           <Link
             className="w-[220px] h-[100px] text-2xl bg-white rounded-xl border-2 border-black shadow-xl hover:shadow flex justify-content items-center"
             to="register"
           >
-            Register
+            {data.lang.register}
           </Link>
         </div>
       </div>
